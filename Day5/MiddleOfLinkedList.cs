ec4a509c7713afaa1445cf35646809e594947840
Abdulrahman Adel <abdoadel791@gmail.com>
2021-11-18 19:59:02 +0200

    Day 5

---
+++ Day5/MiddleOfLinkedList.cs
@@ -0,0 +1,40 @@
+﻿namespace Algorithm_I.Day5
+{
+    public class MiddleOfLinkedList
+    {
+        public ListNode MiddleNode(ListNode head)
+        {
+            var middle = head;
+            var lastNode = head;
+            var tracker = 0;
+
+            while (lastNode != null)
+            {
+                lastNode = lastNode.next;
+                if (tracker == 1)
+                {
+                    middle = middle.next;
+                    tracker--;
+                }
+                else
+                {
+                    tracker++;
+                }
+            }
+
+            return middle;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ Day5/TheKNodeInLinkedListProblem.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Algorithm_I.Day5
+{
+    public class TheKNodeInLinkedListProblem
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            var lastNode = head;
+            var kNode = head;
+            var nodesCount = 1;
+            var tmp = kNode;
+            
+            while (nodesCount <= n && lastNode?.next != null)
+            {
+                lastNode = lastNode?.next;
+                nodesCount++;
+            }
+            
+            while (lastNode?.next != null)
+            {
+                lastNode = lastNode?.next;
+            
+                kNode = kNode?.next;
+                nodesCount++;
+            }
+            
+            if (n == nodesCount)
+            {
+                return tmp.next;
+            }
+            
+            kNode.next = kNode?.next?.next;
+            return tmp;
+        }
+    }
+}
