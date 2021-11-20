acab03491e957b92c6990c3dffb2c5c5f5b503e7
Abdulrahman Adel <abdoadel791@gmail.com>
2021-11-20 18:52:10 +0200

    Day 6

---
+++ Day6/LongestSubstringWithoutRepeatingCharsProblem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithm_I.Day6
+{
+    public class LongestSubstringWithoutRepeatingCharsProblem
+    {
+        /*
+         * Given a string s, find the length of the longest substring without repeating characters.
+         */
+        public static int Run(string s)
+        {
+            var problem = new LongestSubstringWithoutRepeatingCharsProblem();
+            return problem.LengthOfLongestSubstring(s);
+        }
+
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (s.Length == 0) return 0;
+            var hash = new Dictionary<char, int>();
+            var maxLength = 0;
+            for (int start = 0, end = 0; end < s.Length; end++)
+            {
+                var ch = s[end];  // abcabcbb
+
+                if (hash.ContainsKey(ch))
+                {
+                    start = Math.Max(start, hash.GetValueOrDefault(ch) + 1);
+                }
+
+                hash[ch] = end;
+                maxLength = Math.Max(maxLength, end - start + 1);
+            }
+            return maxLength;
+            
+        }
+        
+    }
+}
---
+++ Day6/PermutationInStringProblem.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithm_I.Day6
+{
+    public class PermutationInStringProblem
+    {
+        /*
+         * Given two strings s1 and s2,
+         * return true if s2 contains a permutation of s1,
+         * or false otherwise.
+         * In other words, return true if one of s1's permutations is the substring of s2.
+         */
+        public static bool Run(string s1, string s2)
+        {
+            var problem = new PermutationInStringProblem();
+            return problem.CheckInclusion(s1, s2);
+        }
+
+        public bool CheckInclusion(string s1, string s2)
+        {
+            int len1 = s1.Length, len2 = s2.Length;
+            if (len1 > len2) return false;
+
+            var count = new int[26];
+            // initialize the count for each unique character
+            foreach (var c in s1)
+            {
+                count[c - 'a']++;
+            }
+
+            // initialize the sliding window
+            int i = 0, j = 0, counter = len1;
+            while (j < len1)
+            {
+                if (count[s2[j++] - 'a']-- > 0)
+                {
+                    counter--;
+                }
+            }
+
+            while (j < len2 && counter != 0)
+            {
+                if (count[s2[i++] - 'a']++ >= 0) counter++;
+                if (count[s2[j++] - 'a']-- > 0) counter--;
+            }
+
+            return counter == 0;
+        }
+    }
+}
