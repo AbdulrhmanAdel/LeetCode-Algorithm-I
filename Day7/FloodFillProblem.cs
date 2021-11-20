ec51f9997fc08d376585d8b1f1114c071af2c2f7
Abdulrahman Adel <abdoadel791@gmail.com>
2021-11-20 20:41:42 +0200

    Day 7

---
+++ Day7/FloodFillProblem.cs
@@ -0,0 +1,49 @@
+﻿namespace Algorithm_I.Day7
+{
+    public class FloodFillProblem
+    {
+        /*
+         * An image is represented by an m x n integer grid image where image[i][j] represents the pixel value of the image.
+         * You are also given three integers sr, sc, and newColor. You should perform a flood fill on the image starting from the pixel image[sr][sc].
+         * To perform a flood fill, consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel,
+         * plus any pixels connected 4-directionally to those pixels (also with the same color), and so on. Replace the color of all of the aforementioned pixels with newColor.
+         * Return the modified image after performing the flood fill.
+         */
+        
+        public static int[][] Run(int[][] image, int sr, int sc, int newColor)
+        {
+            var problem = new FloodFillProblem();
+            return problem.FloodFill(image, sr, sc, newColor);
+        }
+        
+        private  int[][] _image;
+        private  bool[,] _visited;
+        private  int _oldColor;
+        private  int _newColor;
+        public int[][] FloodFill(int[][] image, int sr, int sc, int newColor)
+        {
+            _image = image;
+            _visited = new bool[image.Length, image[0].Length];
+            _oldColor = image[sr][sc];
+            _newColor = newColor;
+            Visit(sr, sc);
+            return image;
+        }
+
+        private void Visit(int y, int x)
+        {
+            if (y < 0 || y >= _image.Length || x < 0 || x >= _image[0].Length || _visited[y, x] || _image[y][x] != _oldColor)
+            {
+                return;
+            }
+
+            _image[y][x] = _newColor;
+            _visited[y, x] = true;
+            Visit(y + 1, x);
+            Visit(y - 1, x);
+            Visit(y, x + 1);
+            Visit(y, x - 1);
+        }
+        
+    }
+}
---
+++ Day7/MaxAreaOfIslandProblem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Algorithm_I.Day7
+{
+    public class MaxAreaOfIslandProblem
+    {
+        /*
+         * You are given an m x n binary matrix grid.
+         * An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.)
+         * You may assume all four edges of the grid are surrounded by water.
+         * The area of an island is the number of cells with a value 1 in the island.
+         * Return the maximum area of an island in grid. If there is no island, return 0.
+         */
+
+
+        public static int Run(int[][] grid)
+        {
+            var problem = new MaxAreaOfIslandProblem();
+            return problem.MaxAreaOfIsland(grid);
+        }
+
+        
+        private int[][] _grid;
+        private bool[,] _visited;
+        private int _maxArea;
+        public int MaxAreaOfIsland(int[][] grid)
+        {
+            _grid = grid;
+            _visited = new bool[grid.Length, grid[0].Length];
+            for (var i = 0; i < grid.Length; i++)
+            {
+                for (var j = 0; j < grid[0].Length; j++)
+                {
+                    if (grid[i][j] == 0) continue;
+
+                    var area = Visit(i, j);
+                    _maxArea = Math.Max(area, _maxArea);
+                }
+            }
+
+            return _maxArea;
+        }
+
+        private int Visit(int y, int x)
+        {
+            if (y < 0 || y >= _grid.Length || x < 0 || x >= _grid[0].Length || _visited[y, x] || _grid[y][x] != 1)
+            {
+                return 0;
+            }
+
+            _visited[y, x] = true;
+            return 1 + Visit(y - 1, x) + Visit(y + 1, x) + Visit(y, x + 1) + Visit(y, x - 1);
+        }
+    }
+}
