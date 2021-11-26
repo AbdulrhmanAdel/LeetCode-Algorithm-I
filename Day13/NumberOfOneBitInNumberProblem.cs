31fa73a505f6fde66d8c0bd2559a090545429162
Abdulrahman Adel <abdoadel791@gmail.com>
2021-11-26 02:12:28 +0200

    Day 13

---
+++ Day13/NumberOfOneBitInNumberProblem.cs
@@ -0,0 +1,29 @@
+﻿namespace Algorithm_I.Day13
+{
+    public class NumberOfOneBitInNumberProblem
+    {
+
+        /*
+         * Write a function that takes an unsigned integer
+         * and returns the number of '1' bits it has
+         * (also known as the Hamming weight (http://en.wikipedia.org/wiki/Hamming_weight)).
+         */
+        public static int Run(uint n)
+        {
+            var p = new NumberOfOneBitInNumberProblem();
+            return p.HammingWeight(n);
+        }
+        public int HammingWeight(uint n)
+        {
+            // n & n -1 remove one (1 bit) from n and so on unit n has no 1 so it will become 0
+            var count = 0;
+            while (n != 0)
+            {
+                n &= n - 1;
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ Day13/PowerOfTwoProblem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Algorithm_I.Day13
+{
+    public class PowerOfTwoProblem
+    {
+        /*
+         * Given an integer n, return true if it is a power of two. Otherwise, return false.
+         * An integer n is a power of two, if there exists an integer x such that n == 2x.
+         */
+        public bool IsPowerOfTwo(int n) {
+            // every 2 base number should contains only have 1 bit and number of 0 bits
+            // ex: 1 = 1, 2 = 10, 4 = 100 and if we subtract 1 from n it will have zero bit and 1 bits
+            // ex: 0 = 0, 1, 01, 4 = 011 and with bitwise and to have 1 both should have 1
+            // and this will make 10 & 01 = 00 = 0 int and this is our check
+            return n > 0 && (n & n - 1) == 0;
+            
+            // another solution
+            // get binary of n if first bit != 1 then is not power of two
+            // then check every other bit of its not 0 then its not power of 2
+            if (n < 0) return false;
+            var binary = Convert.ToString(n, 2); // change number to binary
+            if (binary[0] != '1') return false;
+            
+            for (var i = 1; i < binary.Length; i++)
+            {
+                if (binary[i] != '0') return false;
+            }
+            
+            return true;
+            
+        }
+    }
+}
